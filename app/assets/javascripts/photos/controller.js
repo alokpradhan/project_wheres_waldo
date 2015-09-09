6e12bb23187ac2b8d6b03420c743c59408236f26
facingsouth <xin.d.liu@gmail.com>
2015-09-09 09:47:50 -0700

    view.js

--- app/assets/javascripts/photos/controller.js
+++ app/assets/javascripts/photos/controller.js
@@ -1,1 +1,16 @@
 var PHOTOPROCESSOR = PHOTOPROCESSOR || {}
+
+PHOTOPROCESSOR.controller = (function(model, view, $){
+
+  function init() {
+    view.init();
+  }
+
+  return {
+    init: init
+  }
+})(PHOTOPROCESSOR.model, PHOTOPROCESSOR.view, $);
+
+$(document).ready(function(){
+  PHOTOPROCESSOR.controller.init();
+})
--- app/assets/javascripts/photos/view.js
+++ app/assets/javascripts/photos/view.js
@@ -1,1 +1,34 @@
 var PHOTOPROCESSOR = PHOTOPROCESSOR || {}
+
+PHOTOPROCESSOR.view = (function(){
+  function init() {
+    setClickListener();
+  };
+
+  function setClickListener(){
+    $('#photo').click( function(e) {
+      console.log("x " + e.pageX + " y " + e.pageY);
+      makeTags(e.pageX, e.pageY);
+    })
+  };
+
+  function makeTags(x, y) {
+    renderTargetingBox(x, y);
+    // renderDropDown(x, y);
+  };
+
+  function renderTargetingBox(x, y) {
+    var $box = $('<div>');
+    $box.addClass('targeting-box');
+    $box.css({ left: x, top: y });
+  };
+
+  function renderDropDown(x, y) {
+
+  };
+
+
+  return {
+    init: init
+  };
+})();
